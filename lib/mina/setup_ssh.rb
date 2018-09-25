d557430337991778892f4a4268d34765ede82dfc
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-25 13:50:45 +0200

    major rewrite of main class

--- lib/mina/setup_ssh.rb
+++ lib/mina/setup_ssh.rb
@@ -3,7 +3,7 @@
 $LOAD_PATH.unshift("#{__dir__}/..")
 
 module Mina
-  class Mina::SetupSsh < Hash
+  class Mina::SetupSsh
   end
 end
 
@@ -14,64 +14,29 @@ class Mina::SetupSsh
   autoload :Pathname, 'pathname'
 
   # @return [Hash]
-  attr_reader :options
+  attr_reader :config
 
   {
     VERSION: :version,
     Config: :config,
+    Keyring: :keyring
   }.each { |k, v| autoload k, "#{__dir__}/setup_ssh/#{v}" }
 
-  # A new instance of SetupSsh.
-  #
-  # ``ssh_keys`` can be a ``Hash`` where
-  # keys are remote filenames, values are local filepaths.
-  # When a simple copy is expected, an ``Array`` can be used.
-  #
-  # @param [Array<String>|Hash{Symbol|String => String}] ssh_keys
-  # @param [Hash{Symbol => Object}] options
-  def initialize(ssh_keys = {}, options = {})
-    self.options = options.to_hash
-
-    self.class.__send__(:transform, ssh_keys).each do |k, v|
-      self[k] = v
-    end
+  def initialize(config = nil)
+    self.config = (config || Config.new).clone.freeze
   end
 
   # Denote verbose.
   #
   # @return [Boolean]
   def verbose?
-    self.options.fetch(:verbose, false)
+    self.config.fetch(:verbose, false)
   end
 
   protected
 
-  # Options.
+  # Config.
   #
-  # @type [Hash{Symbol => Object}]
-  attr_writer :options
-
-  class << self
-    protected
-
-    # Transform keys to obtain an ``Hash``.
-    #
-    # Keys are remote filenames, values are local filepaths.
-    #
-    # @param [Array<String>|Hash{Symbol|String => String}] ssh_keys
-    # @return [Hash{String => Pathname}]
-    def transform(ssh_keys)
-      if ssh_keys.is_a?(Array)
-        ssh_keys = ssh_keys.map { |v| [Pathname.new(v).basename.to_s, v] }.to_h
-      end
-
-      ssh_keys.map do |k, v|
-        begin
-          [k.to_s, Pathname.new(v).expand_path.realpath]
-        rescue Errno::ENOENT
-          [k.to_s, Pathname.new(v).expand_path]
-        end
-      end.to_h
-    end
-  end
+  # @type [Config]
+  attr_writer :config
 end
