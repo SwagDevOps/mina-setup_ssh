75800e16d8c3490c2cd87fb280d2640c9cb1b4b3
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-25 18:26:29 +0200

    setup_ssh_spec (spec) update

--- spec/mina/setup_ssh_spec.rb
+++ spec/mina/setup_ssh_spec.rb
@@ -5,14 +5,16 @@ require 'pathname'
 # constants
 describe Mina::SetupSsh, :setup_ssh do
   it { expect(described_class).to be_const_defined(:VERSION) }
+  it { expect(described_class).to be_const_defined(:Config) }
+  it { expect(described_class).to be_const_defined(:Keyring) }
 end
 
 # class methods
 describe Mina::SetupSsh, :setup_ssh do
-  it { expect(described_class).to respond_to(:new).with(0).arguments }
   it { expect(described_class).to respond_to(:bundled?).with(0).arguments }
+
+  it { expect(described_class).to respond_to(:new).with(0).arguments }
   it { expect(described_class).to respond_to(:new).with(1).arguments }
-  it { expect(described_class).to respond_to(:new).with(2).arguments }
 
   context '.bundled?' do
     it { expect(described_class.bundled?).to be(true) }
@@ -21,15 +23,13 @@ end
 
 # instance methods
 describe Mina::SetupSsh, :setup_ssh do
-  it { expect(subject).to be_a(Hash) }
-
   it { expect(subject).to respond_to(:verbose?).with(0).arguments }
   it { expect(subject).to respond_to(:verbose?).with(0).arguments }
 end
 
 describe Mina::SetupSsh, :setup_ssh do
   let(:config) { {} }
-  let(:subject) { described_class.new({}, config) }
+  let(:subject) { described_class.new(config) }
 
   context '#verbose?' do
     it { expect(subject.verbose?).to be(false) }
@@ -45,41 +45,5 @@ describe Mina::SetupSsh, :setup_ssh do
     let(:config) { { verbose: true } }
 
     it { expect(subject.verbose?).to be(true) }
-  end
-end
-
-describe Mina::SetupSsh, :setup_ssh do
-  context '.new.empty?' do
-    let(:ssh_keys) { {} }
-
-    it { expect(described_class.new(ssh_keys).empty?).to be(true) }
-  end
-
-  context '.new.empty?' do
-    let(:ssh_keys) { [] }
-
-    it { expect(described_class.new(ssh_keys).empty?).to be(true) }
-  end
-end
-
-describe Mina::SetupSsh, :setup_ssh do
-  sham!(:ssh_keys).array_faker.call.map(&:to_s).tap do |ssh_keys|
-    let(:subject) { described_class.new(ssh_keys) }
-
-    context '#empty?' do
-      it { expect(subject.empty?).to be(false) }
-    end
-
-    context '#size' do
-      it { expect(subject.size).to be(ssh_keys.size) }
-    end
-
-    ssh_keys.each do |ssh_key|
-      context '#fetch()' do
-        let(:k) { File.basename(ssh_key) }
-
-        it { expect(subject.fetch(k)).to be_a(Pathname) }
-      end
-    end
   end
 end
