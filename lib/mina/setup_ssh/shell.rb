8b8f708564bf2b51bb72766892409476536c27c7
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-09-26 19:27:15 +0200

    shell (lib) include verbose module

--- lib/mina/setup_ssh/shell.rb
+++ lib/mina/setup_ssh/shell.rb
@@ -8,13 +8,7 @@ require_relative '../setup_ssh'
 # @see https://github.com/ruby/rake/blob/68ef9140c11d083d8bb7ee5da5b0543e3a7df73d/lib/rake/file_utils.rb#L44
 class Mina::SetupSsh::Shell
   include Mina::SetupSsh::Configurable
-
-  # Denote verbose.
-  #
-  # @return [Boolean]
-  def verbose?
-    self.config.fetch(:verbose, false)
-  end
+  include Mina::SetupSsh::Configurable::Verbose
 
   # Run given (``cmd``) system command.
   #
