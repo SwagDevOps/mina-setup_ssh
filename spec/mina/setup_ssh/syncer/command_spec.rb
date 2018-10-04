71af86c5973e52a2fbc6a85ecae1eece9283571e
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-10-04 08:30:02 +0200

    syncer/command_spec (spec) #to_a + #to_s examples

--- spec/mina/setup_ssh/syncer/command_spec.rb
+++ spec/mina/setup_ssh/syncer/command_spec.rb
@@ -26,4 +26,35 @@ describe Mina::SetupSsh::Syncer::Command, :'syncer/command' do
   it { expect(subject).to respond_to(:key).with(0).arguments }
   it { expect(subject).to respond_to(:variables).with(0).arguments }
   it { expect(subject).to respond_to(:pattern).with(0).arguments }
+  it { expect(subject).to respond_to(:to_s).with(0).arguments }
+  it { expect(subject).to respond_to(:to_a).with(0).arguments }
+end
+
+describe Mina::SetupSsh::Syncer::Command, :'syncer/command' do
+  let(:config) { { domain: 'example.com', user: 'john_doe' } }
+  let(:key) { OpenStruct.new(path: '~/.ssh/foo_rsa', name: 'foo.bar') }
+  let(:subject) { described_class.new(key, config) }
+
+  context '#to_a' do
+    it do
+      ['rsync',
+       '-q',
+       '-e',
+       'ssh -p 22 -o UserKnownHostsFile=/dev/null ' \
+       '-o StrictHostKeyChecking=no -o LogLevel=error',
+       '~/.ssh/foo_rsa',
+       'john_doe@example.com:~/.ssh/foo.bar']
+        .tap { |expected| expect(subject.to_a).to eq(expected) }
+    end
+  end
+
+  context '#to_s' do
+    it do
+      'rsync -q -e ssh\\ -p\\ 22\\ -o\\ ' \
+      'UserKnownHostsFile\\=/dev/null\\ -o\\ ' \
+      'StrictHostKeyChecking\\=no\\ -o\\ LogLevel\\=error ' \
+      '\\~/.ssh/foo_rsa john_doe@example.com:\\~/.ssh/foo.bar'
+        .tap { |expected| expect(subject.to_s).to eq(expected) }
+    end
+  end
 end
