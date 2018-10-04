7caac98bf1f385fe2e8936125119a38c33d601f5
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-10-04 08:40:03 +0200

    license (lib) header added to source files

--- lib/mina/setup_ssh/tasks.rb
+++ lib/mina/setup_ssh/tasks.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 Mina::SetupSsh.new.tap do |setup|
   setup.config.filtered.each { |k, v| set(k, v) }
 end
